5a14918f07be6e12c84f8f87e2f642bf08a9b3ea
Facundo Martinez <facu@spect.ro>
2017-05-05 23:06:43 -0300

    master: 	* Here comes the code

---
+++ mock_generator/__init__.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user_id', type=int, action='store',
+                        help='Account\'s id where data wil be generated')
+    parser.add_argument('username', action='store',
+                        help='Account\'s Username where data will be generated')
+    parser.add_argument('password', action='store',
+                        help='Account\'s Password where data will be generated')
+    parser.add_argument('installation_id', type=int, action='store',
+                        help='Account\'s installation id where data will be generated')
+    parser.add_argument('provider_id', type=int, action='store',
+                        help='Dummy provider from which all the packets will be')
+    parser.add_argument('from_time', action='store',
+                        help='Date since when to generate data')
+    parser.add_argument('to_time', action='store',
+                        help='Date until when to generate data')
+    parser.add_argument('-s', '--server', dest='server', action='store',
+                        help='Server URL where to hit', default='https://tix.innova-red.net')
+    return parser.parse_args()
---
+++ mock_generator/__main__.py
@@ -0,0 +1,35 @@
+import datetime
+import random
+
+import dateutil.parser
+import requests
+from requests.auth import HTTPBasicAuth
+
+from mock_generator import parse_args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    print(args)
+    url = '{server}/api/user/{user_id}/installation/{installation_id}/reports'.format(server=args.server,
+                                                                                      user_id=args.user_id,
+                                                                                      installation_id=args.installation_id)
+    from_time = dateutil.parser.parse(args.from_time)
+    to_time = dateutil.parser.parse(args.to_time)
+    reports_interval = datetime.timedelta(minutes=10)
+    current_time = from_time
+    while current_time < to_time:
+        data = {
+            'timestamp': int(current_time.timestamp()),
+            'version': '1.0.0',
+            'upUsage': random.random(),
+            'downUsage': random.random(),
+            'upQuality': random.random(),
+            'downQuality': random.random(),
+            'providerId': args.provider_id,
+        }
+        print(data)
+        response = requests.post(url, json=data, auth=HTTPBasicAuth(args.username, args.password))
+        if response.status_code not in (200, 204):
+            print(response)
+        current_time = current_time + reports_interval
